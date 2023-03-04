5ae745ba4587d9a4f1775817ddaeb99d479811d2
edwardtsatsu <edwardakorlie73@gmail.com>
2023-03-04 23:58:05 +0000

    update the doc

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import logging
 
 
-logging.warning('This is a warning message')
+
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.warning('This is a warning message')
+    logging.info('This is an info message')
+    logging.debug('This is a debug message')
+    logging.error('This is an error message')
+    logging.critical('This is a critical message')
+
+
+
+if __name__ == '__main__':
+    main()
