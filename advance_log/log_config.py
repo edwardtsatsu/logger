e33253c3e39077bcc4a2944341cb5654fe41aff7
edwardtsatsu <edwardakorlie73@gmail.com>
2023-03-05 03:00:16 +0000

    addeed color to log

--- advance_log/log_config.py
+++ advance_log/log_config.py
@@ -1,20 +1,31 @@
 import logging
 from logging.handlers import SysLogHandler
+from coloredlogs import ColoredFormatter
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
+ANALYZER = os.getenv("ANALYZER")
 PAPERTRAIL_HOST = os.getenv("PAPERTRAIL_HOST")
 PAPERTRAIL_PORT = int(os.getenv("PAPERTRAIL_PORT"))
-ANALYZER = os.getenv("ANALYZER")
-
 
 def create_logger() -> logging.Logger:
     logger = logging.getLogger(ANALYZER)
     logger.setLevel(logging.DEBUG)
-    handler = SysLogHandler(address=(PAPERTRAIL_HOST, PAPERTRAIL_PORT))
-    formatter = logging.Formatter(f'{ANALYZER} %(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
+    handler = SysLogHandler(address=(PAPERTRAIL_HOST, PAPERTRAIL_PORT))
+    handler.setLevel(logging.DEBUG)
+
+    formatter = ColoredFormatter(f'{ANALYZER} %(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
     return logger
+
+
+
+
+
+
+
