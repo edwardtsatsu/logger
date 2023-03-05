6525f4d18397e35dc2bc88d5b84e0b84aeeff02d
edwardtsatsu <edwardakorlie73@gmail.com>
2023-03-05 01:04:13 +0000

    update with papertrail

--- papertrail_log.py
+++ papertrail_log.py
@@ -2,19 +2,32 @@ import logging
 from logging.handlers import SysLogHandler
 
 
-PAPERTRAIL_HOST = "logs.papertrailapp.com"
-PAPERTRAIL_PORT = 37554
+PAPERTRAIL_HOST = "logs5.papertrailapp.com"
+PAPERTRAIL_PORT = 25167
 
 
-def main() -> None:
+def log_message() -> object:
     logger = logging.getLogger("edwardtsatsu")
     logger.setLevel(logging.DEBUG)
     handler = SysLogHandler(address=(PAPERTRAIL_HOST, PAPERTRAIL_PORT))
     logger.addHandler(handler)
 
+    return logger
 
-    logger.debug("This is a debug message")
+
+def even_number(number):
+    try:
+        log = log_message()
+        if number % 2 == 0:
+            log.info(f"ths number {number} is even")
+        else:
+            log.info(f"ths number {number} is odd")
+    except Exception as e:
+        log.error(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
-    main()
+    even_number(5396)
+    even_number(2)
+    even_number(3)
+    even_number("four")
