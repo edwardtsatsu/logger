03028755eb254fec7cafff63f87bd922efe68457
edwardtsatsu <edwardakorlie73@gmail.com>
2023-03-05 00:16:24 +0000

    added the date

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import logging
 def main():
     logging.basicConfig(
         level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s (message)s'
-
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt="%Y-%m-%d %I:%M:%S",
+        filename="basic.log"
     )
+
     logging.warning('This is a warning message')
     logging.info('This is an info message')
     logging.debug('This is a debug message')
